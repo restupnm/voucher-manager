128b34e5d881e123fc79a9590563b1fa83c77b86
chippedpaint_ <gpkwtjbcyt@privaterelay.appleid.com>
2026-06-26 20:25:51 +0900

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -876,6 +876,7 @@ function viewDashboard() {
     ${sidebarHTML()}
     
 <main class="flex-1 min-w-0 w-full p-5 sm:p-8 lg:p-8 lg:ml-64">
+
 <button
 onclick="toggleSidebar()"
 class="lg:hidden mb-5 p-3 rounded-xl bg-white shadow-soft">
